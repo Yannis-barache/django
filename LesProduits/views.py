74ab11746cf2560c988ee7b2017a33383354f30c
Ya.nnis <ybarache@icloud.com>
2024-09-11 12:21:56 +0200

    AJout de truc dans la BD + affichage

--- LesProduits/views.py
+++ LesProduits/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from LesProduits.models import Product
+
+
 # Create your views here.
 
 def index(request):
@@ -8,3 +11,7 @@ def index(request):
 
 def evan(request,name):
     return render(request, 'evan.html', {'name': name})
+
+def ListProducts(request):
+    prdcts = Product.objects.all()
+    return render(request, 'ListProducts.html', {'prdcts': prdcts})
