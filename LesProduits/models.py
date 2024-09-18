e1ce852017e73bfd5c01f019dca8432ec2433dc7
Ya.nnis <ybarache@icloud.com>
2024-09-18 11:28:31 +0200

    Ajout d'un pdf plus tout avant la toolbar est fait

--- LesProduits/models.py
+++ LesProduits/models.py
@@ -1,33 +1,92 @@
 from django.db import models
 from django.utils import timezone
 
-class Statut(models.Model):
+PRODUCT_STATUS = (
+    (0, 'Offline'),
+    (1, 'Online'),
+    (2, 'Out of stock')
+)
 
-    num = models.IntegerField()
+# Create your models here.
+"""
+    Status : numero, libelle
+"""
+
+
+class Status(models.Model):
+    numero = models.IntegerField()
     libelle = models.CharField(max_length=100)
 
-    def __unicode__(self):
-        return "Lib : {0}".format(self.libelle)
+    def __str__(self):
+        return "{0} {1}".format(self.numero, self.libelle)
+
+
+"""
+Produit : nom, code, etc.
+"""
+
 
 class Product(models.Model):
-        name = models.CharField(max_length=100)
-        code = models.IntegerField()
-        price = models.FloatField()
-        date_fabrication = models.DateField(auto_now_add=True)
-        statut = models.ForeignKey(Statut, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Produit"
+
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, null=True, blank=True, unique=True)
+    price_ht = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True,
+                                   verbose_name="Prix unitaire HT")
+    price_ttc = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True,
+                                    verbose_name="Prix unitaire TTC")
+    status = models.SmallIntegerField(choices=PRODUCT_STATUS, default=0)
+    date_creation = models.DateTimeField(blank=True, verbose_name="Date création")
+
+    def __str__(self):
+        return "{0} {1}".format(self.name, self.code)
 
 
+"""
+    Déclinaison de produit déterminée par des attributs comme la couleur, etc.
+"""
 
-        def __unicode__(self):
-            return "{0} [{1}]".format(self.name, self.code)
 
 class ProductItem(models.Model):
-        color = models.CharField(max_length=100)
-        product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Déclinaison Produit"
 
-        def __unicode__(self):
-            return "{0} {{1}} [{2}]".format(self.color, self.product.name, self.code)
+    color = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, null=True, blank=True, unique=True)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    attributes = models.ManyToManyField("ProductAttributeValue", related_name="product_item", null=True, blank=True)
+
+    def __str__(self):
+        return "{0} {1}".format(self.color, self.code)
 
 
+class ProductAttribute(models.Model):
+    """
+    Attributs produit
+    """
+
+    class Meta:
+        verbose_name = "Attribut"
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
+class ProductAttributeValue(models.Model):
+    """
+    Valeurs des attributs
+    """
+
+    class Meta:
+        verbose_name = "Valeur attribut"
+        ordering = ['position']
+
+    value = models.CharField(max_length=100)
+    product_attribute = models.ForeignKey('ProductAttribute', verbose_name="Unité", on_delete=models.CASCADE)
+    position = models.PositiveSmallIntegerField("Position", null=True, blank=True)
+
+    def __str__(self):
+        return "{0} [{1}]".format(self.value, self.product_attribute)
