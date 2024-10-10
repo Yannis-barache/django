433ce647984a61f21806eb9595b274c0e48987c7
Ya.nnis <ybarache@icloud.com>
2024-10-10 09:06:59 +0200

    Changement du pylint

--- firsttuto/settings.py
+++ firsttuto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-%12(+wy**lr(6s^%esf_dotqp+d#b*665=qzxipgzgx@eut8+m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 # Définition de la route LOGIN_URL
 LOGIN_URL = '/lesProduits/login/'
